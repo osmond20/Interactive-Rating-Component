4e6426337695b62051ff08c09d7e11a347f7413f
Nkosinathi Mothudi <osmondmothudi@gmail.com>
2026-08-30 16:18:11 +0200

    added code for CSS, applied minor changes in HTML and implemented JS functionality

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const selectedRate = document.querySelectorAll(".rate");
+const submitBtn = document.querySelector(".rating-submit-button");
+const inputContainer = document.querySelector(".input-container");
+const thankYouContainer = document.querySelector(".thank-you-state-component");
+let rate = 0;
+let rateHolder = document.querySelector(".selected-rate");
+
+selectedRate.forEach(button =>{
+    button.addEventListener("click", () =>{
+        rate = button.textContent;
+        button.classList.add("select");
+    })
+})
+
+submitBtn.addEventListener("click", () =>{
+    inputContainer.classList.add("not-show");
+    rateHolder.textContent = rate;
+    thankYouContainer.classList.add("show");
+})
